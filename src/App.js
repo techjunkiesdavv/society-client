fd953839eab6228f670ab8bc6e01c4886a967cbe
Harsh Farkiya <108735661+HarshhFarkiya@users.noreply.github.com>
2023-03-13 20:43:44 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Society System
+         Society System Test
         </p>
         <a
           className="App-link"
