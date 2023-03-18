b6aa5cbbec42b621b51b8ad7afdb05a1a8ca99bb
Deepak Patil <108725514+DeepakkPatil@users.noreply.github.com>
2023-03-18 18:35:21 +0530

    login D

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,16 +1,18 @@
 import React, {useState} from 'react';
-import login from './login.module.scss';
+import styles from './login.module.scss';
 import logo1 from '../../assets/logo1.png';
-import email from '../../assets/email1.svg';
+import emailImg from '../../assets/email1.svg';
 import eyeopen from '../../assets/show.svg';
 import eyeclose from '../../assets/hide.svg';
 import lock from '../../assets/passw.svg';
-
-
-
+// images folder after merge
 
 const Login = () => {
   const [showpassword, setshowpassword]=useState(false);
+  const [email,setEmail] = useState('') ;
+  const [password,setPassword] = useState('') ;
+  const [isLogin,setIsLogin] = useState(false) ;
+
   function Toggle() {
     var temp = document.getElementById("typepass");
     if (temp.type === "password") {
@@ -21,48 +23,66 @@ const Login = () => {
         temp.type = "password";
         setshowpassword(false);
     }
-}
+    }
+
+    const handleLogin =(e)=>{
+        
+      setIsLogin(true) ;
+        e.preventDefault() ;
+      
+      //call ;
+      setIsLogin(false) ;
+
+    }
+
   return (
-    <div className={login.loginpage}>
-        <form className={login.form}>
-      <img src={logo1} alt='logo' className={login.logo}/>
-      <p className={login.head}>Hello, Welcome back!</p>
-      <div className={login.emailinfo} >
-        <img src={email} alt='email' className={login.emailimg}/>
+    <div className={styles.loginpage}>
+        <form className={styles.form} onSubmit={handleLogin} >
+      <img src={logo1} alt='logo' className={styles.logo}/>
+      <p className={styles.head}>Hello, Welcome back!</p>
+      <div className={styles.emailinfo} >
+        <img src={emailImg} alt='email' className={styles.emailimg}/>
         <input
-          type='text'
-          className={login.email}
-          placeholder='Email'>
-        </input> 
+          type='email'
+          className={styles.email}
+          placeholder='Email' 
+          required
+          onChange={(e)=> setEmail(e.target.value)}
+          value={email} />
       </div> 
-      <div className={login.passinfo} >
-        <img src={lock} alt='password' className={login.passimg}/>
+      <div className={styles.passinfo} >
+        <img src={lock} alt='password' className={styles.passimg}/>
         <input
           type='password' 
-          className={login.pass}
+          className={styles.pass}
           placeholder='Password'
           id="typepass"
-          >
-        </input> 
-        <img src={showpassword?eyeopen:eyeclose} alt='eyeopen' className={login.eyeopen} onClick={Toggle}/>
+          onChange={(e)=> setPassword(e.target.value)}
+          value={password}
+          required
+          />
+     
+        <img src={showpassword?eyeopen:eyeclose} alt='eyeopen' className={styles.eyeopen} onClick={Toggle}/>
       </div>
 
-      <div className={login.remember}>
+      <div className={styles.wrapper}>
+      <div className={styles.remember}>
         <input type="checkbox" className='remembercheck'/>
-        <p className={login.remembertext}>Remember me</p>
+        <p className={styles.remembertext}>Remember me</p>
       </div> 
 
-      <div className={login.forgot}>
+      <div className={styles.forgot}>
         Forgot password?
       </div>
+      </div>
 
-      <button className={login.loginbtn}>
-        Log in
+      <button className={styles.loginbtn}>
+      { isLogin ? 'Logging in':' Log in' } 
       </button>
 
-      <div className={login.register}>
-        <p className={login.text1}>Don't have an account?</p>
-        <p className={login.text2}>Register now!</p>
+      <div className={styles.register}>
+        <p className={styles.text1}>Don't have an account?</p>
+        <p className={styles.text2}>Register now!</p>
       </div>
       </form>
     </div> 
