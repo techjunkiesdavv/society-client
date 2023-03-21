c3ae4e69b181be3ba5d5cf794af51b347f0ebe60
Deepak Patil <108725514+DeepakkPatil@users.noreply.github.com>
2023-03-22 02:11:36 +0530

    resolved conflicts

--- src/Components/Expenditure/BarGraph.js
+++ src/Components/Expenditure/BarGraph.js
@@ -18,9 +18,7 @@ export const BarGraph = () => {
       <YAxis domain={[10, 50]} tick={{ fontSize: 12 }}  ticks={[10000, 20000, 30000, 40000, 50000]} tickFormatter={(tick) => `${tick / 1000}K`}  axisLine={false}/>
       <CartesianGrid horizontal={true} vertical={false} stroke="#9E9B9B" />
       <Tooltip cursor={{fill: 'transparent'}} />
-      <Bar dataKey="expenses" barSize={100} radius={[15, 15, 0, 0]}>
-      <Tooltip cursor={{fill: 'transparent'}} />
-      <Bar dataKey="expenses" barSize={100} radius={[5, 5, 0, 0]}>
+      <Bar dataKey="expenses" barSize={100} radius={[15, 15, 0, 0]} >
         {data.map((entry, index) => (
           <Cell key={`cell-${index}`} fill={entry.color} />
         ))}
