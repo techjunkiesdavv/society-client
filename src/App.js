eb000d5aa1e980bb7697af8efc2edeb5f539d32f
Deepak Patil <108725514+DeepakkPatil@users.noreply.github.com>
2023-03-22 02:22:18 +0530

    server setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import { Complaint } from "./Components";
+import { useEffect, useState } from "react";
+import { fetchData } from "./api/fetch";
 
+
+// check fetch
+
+// const [contact,setContact]= useState([]) ;
+
+//   { contact.map((data,index)=><p key={index}>{data.name} {data.email} { data.message}</p>)}
 function App() {
+
+  const [contact,setContact]= useState([]) ;
+    useEffect(() => {   
+    fetchData('contact').then((data)=>setContact(data)) ;
+  }, [contact]);
+
   return (
     <div>
       <Complaint />
+      { contact.map((data,index)=><p key={index}>{data.name} {data.email} { data.message}</p>)}
     </div>
   );
 }
