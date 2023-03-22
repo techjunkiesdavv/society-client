a83ffa16330670c1a814fdbd2871099358c07214
Deepak Patil <108725514+DeepakkPatil@users.noreply.github.com>
2023-03-23 02:21:03 +0530

    queries

--- src/api/queries.js
+++ src/api/queries.js
@@ -1,10 +1,18 @@
- const Contactquery = '*[_type == "contact"]';
+ const ContactQuery = '*[_type == "contact"]';
+const ExpenditureQuery= '*[_type == "expenditure"]' 
+const CommitteeMembersQuery='*[_type == "committeMembers"]' 
+const AnnouncementsQuery='*[_type == "announcements"]' 
+const AboutQuery='*[_type == "about"]' 
 
  export const getquery=(text)=>{
 
     switch(text)
     {
-        case  'contact':    return Contactquery ;
+        case  'contact':    return ContactQuery ;
+        case 'expenditure' : return ExpenditureQuery ;
+        case 'committe_members' : return CommitteeMembersQuery ;
+        case 'about' : return AboutQuery ;
+        case 'announcements' : return AnnouncementsQuery
 
         default : return -1 ; // check 
     }
