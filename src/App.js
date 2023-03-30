9969e9e9bbfb749ef3c1952acfd2ccb0e356c022
Deepak Patil <108725514+DeepakkPatil@users.noreply.github.com>
2023-03-30 17:23:08 +0530

    queries added

--- src/App.js
+++ src/App.js
@@ -1,45 +1,66 @@
-import { Complaint,Login,About,Profile,Sidebar,Expenditure,Committee,Announcement,AnnouncementHead,Facilitycontacts ,Bills} from "./Components";
-import './App.module.scss';
-import styles from './App.module.scss';
+import {
+  Complaint,
+  Login,
+  About,
+  Profile,
+  Sidebar,
+  Expenditure,
+  Committee,
+  Announcement,
+  AnnouncementHead,
+  Facilitycontacts,
+  Bills,
+} from "./Components";
+import "./App.module.scss";
+import styles from "./App.module.scss";
 import { useEffect, useState } from "react";
 import { fetchData } from "./api/fetch";
 
 // check fetch
 
-// const [contact,setContact]= useState([]) ;
-
-//   { contact.map((data,index)=><p key={index}>{data.name} {data.email} { data.message}</p>)}
-
 function App() {
-  const [contact,setContact]= useState([]) ;
-    useEffect(() => {   
-    fetchData('contact').then((data)=>setContact(data)) ;
-  }, [contact]);
-  const [page,setPage]=useState("Home");
-  const [login,setLogin]=useState(false);
+  const [contact, setContact] = useState([]);
+
+  // text for queries are:-
+
+  //     contact
+  // expenditure
+  // committe_members
+  // about
+  // announcements
+  // facilityContacts
+  // bills
+
+  useEffect(() => {
+    fetchData("expenditure").then((data) => setContact(data));
+  }, []);
+
+  console.log(contact);
+  const [page, setPage] = useState("Home");
+  const [login, setLogin] = useState(false);
   return (
     <div className={styles.container}>
       <div className={styles.sidebar}>
-      <Sidebar setPage={setPage}/>
+        <Sidebar setPage={setPage} />
       </div>
       <div className={styles.home}>
-     {page==="Home"&&<AnnouncementHead/>}
-     {page==="Home"&&<About/>}
-     {page==="Home"&&<Complaint/>}
-     {page==="Complaint"&&<Complaint/>}
-     {page==="Home"&&<Committee/>}
-      {page==="Expenditure"&&<Expenditure/>}
-      {page==="Announcement"&&<Announcement/>}
-      {page==="Home" && <Bills />}
-      { page === "Home" && <Facilitycontacts />}
-      {page==="Facility Contact"&&<Facilitycontacts/>}
-      {/* <Register/> */}
+        {page === "Home" && <AnnouncementHead />}
+        {page === "Home" && <About />}
+        {page === "Home" && <Complaint />}
+        {page === "Complaint" && <Complaint />}
+        {page === "Home" && <Committee />}
+        {page === "Expenditure" && <Expenditure />}
+        {page === "Announcement" && <Announcement />}
+        {page === "Home" && <Bills />}
+        {page === "Home" && <Facilitycontacts />}
+        {page === "Facility Contact" && <Facilitycontacts />}
+        {/* <Register/> */}
       </div>
       <div className={styles.profile}>
-      {login?<Profile/>:<Login setLogin={setLogin}/>}
+        {login ? <Profile /> : <Login setLogin={setLogin} />}
       </div>
-     
-      { contact.map((data,index)=><p key={index}>{data.name} {data.email} { data.message}</p>)}
+
+      {/* { contact.map((data,index)=><p key={index}>{data.name} {data.email} { data.message}</p>)} */}
     </div>
   );
 }
