1284b7f71cb3974f7a35ec098742d00118624dd8
Suhani2402 <108735048+Suhani2402@users.noreply.github.com>
2023-04-11 20:16:38 +0530

    Funds

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Complaint } from "./Components";
 import {Register} from "./Components";
+import {Funds} from "./Components";
 
 function App() {
   return (
     <div>
       <Complaint />
       <Register/>
+      <Funds/>
     </div>
   );
 }
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,5 @@
 import Complaint from "./Complaint/Complaint";
 import Register from "./Register/Register";
+import Funds from "./Funds/Funds";
 
-export { Complaint,Register };
+export { Complaint,Register,Funds };
