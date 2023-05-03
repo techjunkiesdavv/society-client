f6983b3834be90977042de198ef9d7e0e88f3026
Deepak Patil <108725514+DeepakkPatil@users.noreply.github.com>
2023-05-03 20:57:03 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({baseURL:'http://localhost:5000'});
+const API = axios.create({baseURL:'https://server-mongo-gamma.vercel.app'});
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
       req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
